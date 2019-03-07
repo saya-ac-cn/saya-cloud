0a413d22340cf9779f921c7ce998ef3847a73178
liunengkai <saya@saya.ac.cn>
2019-03-07 23:30:43 +0800

    修改后台布局

--- src/views/frontend/index.vue
+++ src/views/frontend/index.vue
@@ -1,5 +1,6 @@
 <template>
   <el-container class="this-container">
+    <div class="background"></div>
     <el-header class="this-header"></el-header>
     <el-main class="this-main"> Main</el-main>
     <el-footer class="this-copright">Footer</el-footer>
@@ -26,23 +27,32 @@
     top: 0px;
     bottom: 0px;
     width: 100vw;
-    /* 加载背景图 */
-    background-image: url('../../assets/picture/login/login_background1.jpg');
-    /* 背景图垂直、水平均居中 */
-    background-position: center center;
-    /* 背景图不平铺 */
-    background-repeat: no-repeat;
-    /* 当内容高度大于图片高度时，背景图像的位置相对于viewport固定 */
-    background-attachment: fixed;
-    /* 让背景图基于容器大小伸缩 */
-    background-size: cover;
-    background-attachment: fixed;
     overflow-x: hidden;
     overflow-y: hidden;
-    .this-header{
-      height: 200px;
+    .background{
+      position: absolute;
+      z-index: -999;
       width: 100vw;
-      background-color: #752B7D;
+      top: 0px;
+      bottom: 0px;
+      /* 加载背景图 */
+      background-image: url('../../assets/picture/login/login_background1.jpg');
+      /* 背景图垂直、水平均居中 */
+      background-position: center center;
+      /* 背景图不平铺 */
+      background-repeat: no-repeat;
+      /* 当内容高度大于图片高度时，背景图像的位置相对于viewport固定 */
+      background-attachment: fixed;
+      /* 让背景图基于容器大小伸缩 */
+      background-size: cover;
+      background-attachment: fixed;
+    }
+    .this-header{
+      height: 70px;
+      opacity: 0.8;
+      filter: alpha(opacity=70);
+      width: 100vw;
+      background-color: #000;
     }
     .this-main{
       width: 100vw;
@@ -56,7 +66,9 @@
     .this-copright{
       width: 100vw;
       height: 100px;
-      background-color: #3399ff;
+      opacity: 0.8;
+      filter: alpha(opacity=70);
+      background-color: #000;
     }
   }
 </style>
