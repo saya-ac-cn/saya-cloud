448243c6f1ae4cc3d8c4eab1332b4710383a0493
liunengkai <saya@saya.ac.cn>
2019-07-03 23:48:29 +0800

    增加行政区域查询

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,7 @@ import news from '@/views/frontend/news'
 import plan from '@/views/frontend/plan'
 import board from '@/views/frontend/board'
 import note from '@/views/frontend/note'
+import query from '@/views/frontend/query'
 import newsView from '@/views/frontend/news-view'
 import notesView from '@/views/frontend/notes-view'
 import NotFound from '@/views//404.vue'
@@ -65,6 +66,7 @@ export default new Router({
         { path: '/note', component: note, name: '随笔记录', hidden: false ,meta:{title: baseName + '随笔记录',requireAuth: false}},
         { path: '/plan', component: plan, name: '计划安排', hidden: false ,meta:{title: baseName + '计划安排',requireAuth: false}},
         { path: '/board', component: board, name: '留言反馈', hidden: false ,meta:{title: baseName + '留言反馈',requireAuth: false}},
+        { path: '/query', component: query, name: '接口查询', hidden: false ,meta:{title: baseName + '接口查询',requireAuth: false}},
         { path: '/news/info', component: newsView, name: '动态详情', hidden: true ,meta:{title: baseName + '动态详情',requireAuth: false}},
         { path: '/notes/info', component: notesView, name: '笔记详情', hidden: true ,meta:{title: baseName + '笔记详情',requireAuth: false}}
       ]
---
+++ src/views/frontend/query.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="base-content">
+    <div class="container">
+      <div class="menu-title">
+        <div class="menu-name">
+          <i class="el-icon-caret-right"></i>接口查询
+        </div>
+      </div>
+      <el-tabs tab-position="left">
+        <el-tab-pane label="行政区域查询-1">
+            <span>行政区域查询（省/直辖市/自治区、市、县/区）</span>
+            <el-form :inline="true" :model="formInline" class="mca-form">
+              <el-form-item  size="small" label="审批人">
+                <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+              </el-form-item>
+              <el-form-item size="small" label="审批人">
+                <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+              </el-form-item>
+              <el-form-item size="small" label="活动区域">
+                <el-select v-model="formInline.region" placeholder="活动区域">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item size="small">
+                <el-button type="primary">查询</el-button>
+              </el-form-item>
+            </el-form>
+            <div class="tip">
+              <p>
+              说明：此接口根据省/直辖市/自治区、市、县/区，三级联动的数据查询方式。数据来源于国家民政部2018年数据，本系统中的数据根据国家民政部每年
+                年底汇总后的数据，整理统计好后同步更新。
+              </p>
+            </div>
+        </el-tab-pane>
+        <el-tab-pane label="行政区域查询-2">配置管理</el-tab-pane>
+        <el-tab-pane label="敬请期待">敬请期待</el-tab-pane>
+      </el-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {validater} from '../../vars/validater'
+  import { writeboard } from '../../api/api';
+  export default {
+    name: 'query',
+    data(){
+      return{
+        formInline: {
+          user: '',
+          region: ''
+        }
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .tip {
+    padding: 8px 16px;
+    background-color: #ecf8ff;
+    border-radius: 4px;
+    border-left: 5px solid #50bfff;
+    margin: 20px 0;
+  }
+  .mca-form{
+    margin-top: 1em;
+  }
+</style>
