b9a30e807103cc419bf495aaac19e55ec54f2a64
liunengkai <saya@saya.ac.cn>
2019-07-04 23:37:40 +0800

    完善行政区域查询

--- src/views/frontend/query.vue
+++ src/views/frontend/query.vue
@@ -9,31 +9,64 @@
       <el-tabs tab-position="left">
         <el-tab-pane label="行政区域查询-1">
             <span>行政区域查询（省/直辖市/自治区、市、县/区）</span>
-            <el-form :inline="true" :model="formInline" class="mca-form">
-              <el-form-item  size="small" label="审批人">
-                <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            <el-form :inline="true" :model="mca.select" class="mca-form">
+              <el-form-item  size="small" label="省/直辖市/自治区">
+                <el-input v-model="mca.select.province" placeholder="审批人"></el-input>
               </el-form-item>
-              <el-form-item size="small" label="审批人">
-                <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+              <el-form-item size="small" label="市">
+                <el-input v-model="mca.select.city" placeholder="审批人"></el-input>
               </el-form-item>
-              <el-form-item size="small" label="活动区域">
-                <el-select v-model="formInline.region" placeholder="活动区域">
+              <el-form-item size="small" label="县/区">
+                <el-select v-model="mca.select.distinct" placeholder="活动区域">
                   <el-option label="区域一" value="shanghai"></el-option>
                   <el-option label="区域二" value="beijing"></el-option>
                 </el-select>
               </el-form-item>
-              <el-form-item size="small">
-                <el-button type="primary">查询</el-button>
-              </el-form-item>
             </el-form>
             <div class="tip">
               <p>
-              说明：此接口根据省/直辖市/自治区、市、县/区，三级联动的数据查询方式。数据来源于国家民政部2018年数据，本系统中的数据根据国家民政部每年
+                说明：此接口根据省/直辖市/自治区、市、县/区，三级联动的数据查询方式。数据来源于国家民政部2018年数据，本系统中的数据根据国家民政部每年
                 年底汇总后的数据，整理统计好后同步更新。
+              </p>
+              <p>
+                注意：当前系统中主要维护了中国🇨🇳大陆各省市的行政区域数据，港澳台地区数据暂缺。由于数据接口服务缺乏专人对服务器、网络的7×24小时维护，
+                可能会对服务的稳定性造成影响。请勿用于生产环境。
               </p>
             </div>
         </el-tab-pane>
-        <el-tab-pane label="行政区域查询-2">配置管理</el-tab-pane>
+        <el-tab-pane label="行政区域查询-2">
+          <span>行政区域查询（省/直辖市/自治区、市、县/区、镇、村）</span>
+          <el-form :inline="true" :model="formInline" class="mca-form">
+            <el-form-item  size="small" label="省/直辖市/自治区">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item size="small" label="市">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item size="small" label="县/区">
+              <el-select v-model="formInline.region" placeholder="活动区域">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item size="small" label="镇">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+            <el-form-item size="small" label="村">
+              <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+            </el-form-item>
+          </el-form>
+          <div class="tip">
+            <p>
+              说明：此接口根据省/直辖市/自治区、市、县/区、镇、村，五级联动的数据查询方式。数据来源于国家统计局2018年数据，本系统中的数据根据国家统计局每年
+              年底汇总后的数据，整理统计好后同步更新。
+            </p>
+            <p>
+              注意：当前系统中主要维护了中国🇨🇳大陆各省市的行政区域数据，港澳台地区数据暂缺。由于数据接口服务缺乏专人对服务器、网络的7×24小时维护，
+              可能会对服务的稳定性造成影响。请勿用于生产环境。
+            </p>
+          </div>
+        </el-tab-pane>
         <el-tab-pane label="敬请期待">敬请期待</el-tab-pane>
       </el-tabs>
     </div>
@@ -50,6 +83,14 @@
         formInline: {
           user: '',
           region: ''
+        },
+        mca:{
+          api:[],
+          select:{
+            province:'',
+            city:'',
+            distinct:''
+          },
         }
       }
     },
